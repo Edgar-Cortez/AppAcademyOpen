f4d141c0606b096921500c4803c6c498846e8f51
Edgar Cortez-Castellano <eac0001@dcccd.edu>
2019-10-24 09:29:16 -0500

    Simple method that takes in a string and manipulates the output

---
+++ Intro to Programming/GoodbyeProblem.rb
@@ -0,0 +1,30 @@
+# Write a method goodbye(name) that takes in a string name and returns a string saying bye to that name. See the example calls.
+
+
+# Steps:
+# 	- Take in a string
+# 	- Append "Bye " to the string the beginning of the string and "." to the end of the string
+# 	- Return the string
+
+
+
+# ---------- My Attempt  ----------
+def goodbye(name)
+	str = "Bye " + name + "."
+         return str
+end
+
+
+
+# ---------- Their Solution ----------
+# They did it all in one line which is better.
+# def goodbye(name)
+#   return "Bye " + name + "."	 
+# end
+
+
+#  ---------- Tests ----------
+puts goodbye("Daniel")   # => "Bye Daniel."
+puts goodbye("Mark")     # => "Bye Mark."
+puts goodbye("Beyonce")  # => "Bye Beyonce."
+
