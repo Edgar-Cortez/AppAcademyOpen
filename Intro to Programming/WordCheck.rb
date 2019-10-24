ea6e4cc7834953ede702a521cef651866341cbe7
Edgar Cortez-Castellano <eac0001@dcccd.edu>
2019-10-24 10:02:32 -0500

    Wrong code. Updated with correct code.

--- Intro to Programming/WordCheck.rb
+++ Intro to Programming/WordCheck.rb
@@ -1,4 +1,4 @@
-# Write a method number_check(num) that takes in a number and returns a string. The method should return the string 'positive' if the num is positive, 'negative' if the num is negative, and 'zero' if the num is zero.
+# Write a method word_check(word) that takes in a word and returns a string. The method should return the string "long" if the word is longer than 6 characters, "short" if it is less than 6 characters, and "medium" if it is exactly 6 characters long.
 
 
 # ---------- Steps ----------
@@ -8,15 +8,16 @@
 
 
 # ---------- My Attempt ----------
-def number_check(num)
-	if num > 0
-		return "positive"
-	elsif num < 0
-		return "negative"
+def word_check(word)
+	if word.length > 6
+		return "long"
+	elsif word.length < 6
+		return "short"
 	else
-		return "zero"
+		return "medium"
 	end
 end
+
 
 
 
@@ -24,6 +25,4 @@ end
 
 
 #  ---------- Tests ----------
-puts number_check(5)    # => "positive"
-puts number_check(-2)   # => "negative"
-puts number_check(0)    # => "zero"
+
