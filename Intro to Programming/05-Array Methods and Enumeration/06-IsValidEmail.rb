364a324054701ed87133191f616a94f11505ede1
EdgarCortez1 <ea.cortezz@gmail.com>
2019-11-06 08:57:43 -0600

    Setup the project

--- Intro to Programming/05-Array Methods and Enumeration/06-IsValidEmail.rb
+++ Intro to Programming/05-Array Methods and Enumeration/06-IsValidEmail.rb
@@ -1,17 +1,30 @@
-# 
+# Write a method is_valid_email that takes in a string and returns a boolean
+# indicating whether or not it is a valid email address. 
 
 # ---------- Steps ----------
 # 	- 
 # 	- 
 # 	- 
 
+# ---------- Hint ----------
+# For simplicity, we'll consider an email valid when it satisfies all of the following:
+# - contains exactly one @ symbol
+# - contains only lowercase alphabetic letters before the @
+# - contains exactly one . after the @
 
 # ---------- My Attempt ----------
+def is_valid_email(str)
 
+end
 
 
 # ---------- Their Solution ----------
 
 
 #  ---------- Tests ----------
-
+puts is_valid_email("abc@xy.z")         # => true
+puts is_valid_email("jdoe@gmail.com")   # => true
+puts is_valid_email("jdoe@g@mail.com")  # => false
+puts is_valid_email("jdoe42@gmail.com") # => false
+puts is_valid_email("jdoegmail.com")    # => false
+puts is_valid_email("az@email")         # => false
