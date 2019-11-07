ab6edccd960db004039a5d19ad560b972fd289d4
EdgarCortez1 <ea.cortezz@gmail.com>
2019-11-07 10:15:27 -0600

    Set up the project

---
+++ Intro to Programming/05-Array Methods and Enumeration/08-ReverseWords.rb
@@ -0,0 +1,26 @@
+# Write a method reverse_words that takes in a sentence string and returns the
+# sentence with the order of the characters in each word reversed. Note that we
+# need to reverse the order of characters in the words, do not reverse the order
+# of words in the sentence.
+
+# ---------- Steps ----------
+# 	- Create empty array
+# 	- Grab string and convert to an array of elements by word
+# 	- While loop through array or Do Loop
+# 		- at each iteration reverse the string
+# 		- push to the empty array
+# 	- Convert new array back to string
+
+
+# ---------- My Attempt ----------
+def reverse_words(sent)
+
+end
+
+
+# ---------- Their Solution ----------
+
+
+#  ---------- Tests ----------
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
