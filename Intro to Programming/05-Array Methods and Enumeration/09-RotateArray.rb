52f8391b86616c00efbff1f4c53df09e98ea2cf2
EdgarCortez1 <ea.cortezz@gmail.com>
2019-11-08 14:48:30 -0600

    Setup project

---
+++ Intro to Programming/05-Array Methods and Enumeration/09-RotateArray.rb
@@ -0,0 +1,28 @@
+# Write a method rotate_array that takes in an array and a number. The method
+# should return the array after rotating the elements the specified number of
+# times. A single rotation takes the last element of the array and moves it to
+# the front. 
+
+# ---------- Steps ----------
+# 	- 
+# 	- loop push/pop/shift/unshift based on number parameter
+# 	- 
+# 	- return new array
+
+
+# ---------- My Attempt ----------
+
+def rotate_array(arr, num)
+
+end
+
+
+# ---------- Their Solution ----------
+
+
+#  ---------- Tests ----------
+print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
+puts
+
+print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
+puts
