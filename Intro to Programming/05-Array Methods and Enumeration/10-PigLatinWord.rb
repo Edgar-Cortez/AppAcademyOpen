f1e1cd706651e6db5a0640a0fa9137ebf5addbf1
EdgarCortez1 <ea.cortezz@gmail.com>
2019-11-15 10:02:28 -0600

    Do this again. I need to double check and find the methods I can use

--- Intro to Programming/05-Array Methods and Enumeration/10-PigLatinWord.rb
+++ Intro to Programming/05-Array Methods and Enumeration/10-PigLatinWord.rb
@@ -2,26 +2,65 @@
 # word into pig latin. 
 
 # ---------- Steps ----------
-# 	- Create an empty array
 # 	- Create a string that holds all the vowels to check from
 # 	- Check letter at index 0
 # 		- If vowel, add 'yay' to the end
-# 		- else, move all letters before the first vowel to the end of the word and add 'ay'
-# 	- After the check shovel the changes into empty array
-# 	- Convert to string
+
+# 		- Use .include? and .with_index
+# 		- else if no vowel at index 0, iterate through until you find the first vowel
+# 			- grab character before the vowel
+# 			- grab character after the vowel and include the vowel
+# 	- Build string and add "ay"
 
 # Pig latin translation uses the following rules:
 # - for words that start with a vowel, add 'yay' to the end
 # - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
 
 # ---------- My Attempt ----------
-def pig_latin_word(word)
+# This is as far as I got
 
-end
+# def pig_latin_word(word)
+# 	translated = []
+# 	vowels = "aeiou"
+# 	convert = word.split(" ")
+
+# 	convert.each_char do |char|
+# 		if !vowels.include?(char)
+# 	  		return false
+# 		end
+# 	end
+
+# end
 
 
 # ---------- Their Solution ----------
+def pig_latin_word(word)
+  vowels = "aeiou"
 
+  # First test case
+  # if the word starts with the vowel, then add yay
+  if vowels.include?(word[0])
+  	return word + "yay"
+  end
+
+  # Second test case
+  # Iterate through the string and the index until you find the first vowel
+  word.each_char.with_index do |char, i|
+  	puts char
+
+  	# We found first vowel in the string
+  	if vowels.include?(char)
+  		# Grabs the characters before the first vowel
+  		before_vowel = word[0...i]
+  		# Grabs the character after the first vowel (includes it as well)
+  		after_vowel = word[i..-1]
+
+  		puts before_vowel
+  		puts after_vowel
+  		return after_vowel + before_vowel + "ay"
+  	end
+  end
+end
 
 #  ---------- Tests ----------
 puts pig_latin_word("apple")   # => "appleyay"
