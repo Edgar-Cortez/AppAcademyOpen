ecd755cd84f9880d452fe6b8c9f80b23a3439855
EdgarCortez1 <ea.cortezz@gmail.com>
2020-07-17 13:37:41 -0500

    Completed lecture 07

---
+++ Intro to Programming/06-Hashes/Lecture/07-Nil.rb
@@ -0,0 +1,16 @@
+# nil
+# represents "nothing" or "empty"
+# commonly appears as a default value
+
+# my_array = ["a", "b", "c"]
+
+# p my_array[2]         # c
+# p my_array[3]         # nil - doesn't exist
+
+my_hash = {
+    "name" => "Edgar",
+    "age" => 30
+}
+
+p my_hash["name"]       # Edgar
+p my_hash["is_cool"]    # nil - doesn't exist
