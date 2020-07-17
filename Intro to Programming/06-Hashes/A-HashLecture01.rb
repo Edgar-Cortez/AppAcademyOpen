c8372a2c621e2dcf125e2f1b0d10720511886d06
EdgarCortez1 <ea.cortezz@gmail.com>
2020-07-17 12:08:50 -0500

    Completed lecture 1

---
+++ Intro to Programming/06-Hashes/A-HashLecture01.rb
@@ -0,0 +1,16 @@
+# Data Structure - Hashes
+# 'key => value' pairs
+# A hash is an object in js terms
+
+my_hash = {
+    "name" => "Edgar",
+    "race" => "white",
+    "age" => "30",
+    "isAwesome" => "true",
+    42 => "hello",
+}
+
+puts my_hash
+puts my_hash["age"]
+puts my_hash["isAwesome"]
+puts my_hash[42]
