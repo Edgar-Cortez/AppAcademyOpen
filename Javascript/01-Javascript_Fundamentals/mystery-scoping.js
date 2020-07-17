21b6bb884f9341f7dba0708d373425fdacb9a326
EdgarCortez1 <ea.cortezz@gmail.com>
2020-07-17 11:47:13 -0500

    Started assignment

--- Javascript/01-Javascript_Fundamentals/mystery-scoping.js
+++ Javascript/01-Javascript_Fundamentals/mystery-scoping.js
@@ -19,14 +19,18 @@ function mysteryScoping2() {
     console.log(x);
 }
 
+// in block
+// out of block
+
 function mysteryScoping3() {
     const x = 'out of block';
     if (true) {
-      var x = 'in block';  
-      console.log(x);
+        var x = 'in block';  
+        console.log(x);
     }
     console.log(x);
 }
+
 // You can't re-declare a const variable with a var
 
 function mysteryScoping4() {
@@ -37,6 +41,7 @@ function mysteryScoping4() {
     }
     console.log(x);
 }
+
 // in block
 // out of block
 
@@ -49,4 +54,12 @@ function mysteryScoping5() {
     let x = 'out of block again';
     console.log(x);
 }
+
 // you can't re-declare a let variable
+
+
+mysteryScoping1();
+mysteryScoping2();
+mysteryScoping3();
+mysteryScoping4();
+mysteryScoping5();
