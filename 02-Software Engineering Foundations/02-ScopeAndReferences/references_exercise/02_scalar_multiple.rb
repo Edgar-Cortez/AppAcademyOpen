ca0977801ee42a21118e7e527229e046797146f1
EdgarCortez1 <ea.cortezz@gmail.com>
2020-08-20 11:07:06 -0500

    Completed

--- 02-Software Engineering Foundations/02-ScopeAndReferences/references_exercise/02_scalar_multiple.rb
+++ 02-Software Engineering Foundations/02-ScopeAndReferences/references_exercise/02_scalar_multiple.rb
@@ -7,7 +7,10 @@
 # to be the same before and after calling your method.
 
 def scalar_multiple!(array, multiplier)
-
+    array.map! do |ele|
+        ele * multiplier
+    end
+    return array
 end
 
 
