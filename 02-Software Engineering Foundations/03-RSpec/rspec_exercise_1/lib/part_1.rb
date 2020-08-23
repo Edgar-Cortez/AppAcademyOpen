fbc178b1d8f568abab000898a951388ba34af2eb
ecortez <ea.cortezz@gmail.com>
2020-08-22 21:59:30 -0500

    Completed part 1

--- 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_1.rb
+++ 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_1.rb
@@ -2,7 +2,28 @@ def average(num_1, num_2)
   (num_1 + num_2) / 2.0
 end
 
-def average_array(*array)
-  sum = array.sum
-  return sum / array.length
+def average_array(array)
+  array.sum / (array.length * 1.0)
+end
+
+def repeat(str, num)
+  str * num
+end
+
+def yell (str)
+  str.upcase + "!"
+end
+
+def alternating_case(str)
+  arr = str.downcase.split(' ')
+
+  arr.each_with_index do |ele, i|
+    if !i.odd?
+      ele.upcase!
+    # else
+    #   ele.downcase!
+    end
+  end
+  
+  return arr.join(' ')
 end
