c60b71d59d98bfe54813f197d175a2be642d32fd
EdgarCortez1 <ea.cortezz@gmail.com>
2020-08-24 12:09:35 -0500

    Completed exercise 2

--- 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_2.rb
+++ 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_2.rb
@@ -1,4 +1,17 @@
 def hipsterfy(word)
-  vowels = "aeiou"
   word.reverse.sub(/[aeiou]/,'').reverse 
+end
+
+def vowel_counts(str)
+  str_hash = Hash.new(0)
+  array = str.downcase.split('')
+  vowels = "aeiouAEIOU"
+
+  array.each do |ele|
+    if vowels.include?(ele)
+      str_hash[ele] += 1
+    end
+  end
+
+  str_hash
 end
