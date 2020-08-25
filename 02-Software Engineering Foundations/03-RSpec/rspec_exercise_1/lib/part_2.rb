2f7456bf3cf282dc8bdbc37803ff0ae6bdeb50b7
EdgarCortez1 <ea.cortezz@gmail.com>
2020-08-24 19:09:17 -0500

    Set up exercise 2

--- 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_2.rb
+++ 02-Software Engineering Foundations/03-RSpec/rspec_exercise_1/lib/part_2.rb
@@ -1,5 +1,20 @@
 def hipsterfy(word)
   word.reverse.sub(/[aeiou]/,'').reverse 
+
+
+  # Given Solution
+  # vowels = "aeiou"
+
+  # i = word.length -1
+  # while i >= 0
+  #   if vowels.include?(word[i])
+  #     return word[0...i] + word[i+1..-1]
+  #   end
+
+  #   i -= 1
+  # end
+
+  # word
 end
 
 def vowel_counts(str)
@@ -14,4 +29,49 @@ def vowel_counts(str)
   end
 
   str_hash
+
+  # Given Solution
+  # counts = Hash.new(0)
+  # vowels = "aeiou"
+
+  # str.each_char do |char|
+  #   if vowels.include?(char.downcase)
+  #     count[char.downcase] += 1
+  #   end
+  # end
+
+  # counts
+end
+
+def caesar_cipher(str, num)
+  # alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  # new_str = ""
+
+  # str.each_char do |char|
+  #     old_idx = alphabet.index(char)
+  #     new_idx = old_idx + num
+  #     new_char = alphabet[new_idx % 26]
+  #     new_str += new_char
+
+  #     new_str += char
+  # end
+
+  # return new_str
+
+  # Given Solution
+  alphabet = ("a".."z").to_a
+  new_str = ""
+
+  str.each_char do |char|
+    if alphabet.include?(char)
+      old_idx = alphabet.index(char)
+      new_idx = old_idx + num
+      new_str += alphabet[new_idx % 26]
+    else
+      new_str += char
+    end
+    
+  end
+
+  new_str
 end
