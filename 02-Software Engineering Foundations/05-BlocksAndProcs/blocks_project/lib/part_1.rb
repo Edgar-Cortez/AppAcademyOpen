649230f075c8f13a86577007e3e2857afb9e802a
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-01 09:47:35 -0500

    Completed reject_puppies exercise

--- 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_1.rb
+++ 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_1.rb
@@ -1,9 +1,16 @@
 require "byebug"
 
 def select_even_nums(arr)
-    even = arr.select do |ele|
-        ele.even?
-    end
+    # even = arr.select do |ele|
+    #     ele.even?
+    # end
+    even = arr.select(&:even?)
 
     even
+end
+
+def reject_puppies(arr)
+    older_pups = arr.reject {|k| k["age"] <= 2}
+
+    older_pups
 end
