af3c7b7f3bdc3661c365edc38164e04b1faa07f7
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-01 15:59:13 -0500

    Redo no_valid_url?. On the right path

--- 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_2.rb
+++ 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_2.rb
@@ -9,5 +9,22 @@ def all_words_capitalized?(arr)
             false
         end
     end
+
+    # Given Solution
+    # arr.all? { |word| word == word.capitalize }
 end
+
+# 2
+def no_valid_url?(arr)
+    valid_domain = ['.com', '.net', '.io', '.org']
+
+    # arr.none? { |url| valid_domain.include? url }
+    
+    # Given Solution
+    arr.none? do |url| 
+       valid_domain.any? { |ending| url.end_with?(ending) }
+    end
+end
+
+
 
