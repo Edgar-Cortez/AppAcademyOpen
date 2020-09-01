fb7fe7e656dc0d150b4a67d018488a2102cecf88
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-01 16:49:13 -0500

    Completed my_select

--- 02-Software Engineering Foundations/05-BlocksAndProcs/procs_project/lib/part_1.rb
+++ 02-Software Engineering Foundations/05-BlocksAndProcs/procs_project/lib/part_1.rb
@@ -8,3 +8,14 @@ def my_map(arr, &prc)
 
     new_arr
 end
+
+def my_select(arr, &prc)
+    new_arr = []
+    arr.each do |ele|
+        if prc.call(ele) == true
+            new_arr << ele
+        end
+    end
+
+    new_arr
+end
