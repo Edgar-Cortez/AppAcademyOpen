8fe57ba30d4e5ae21642c354315fae6295f6c3d4
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-01 14:37:51 -0500

    Redo count_positive_subarrays

--- 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_1.rb
+++ 02-Software Engineering Foundations/05-BlocksAndProcs/blocks_project/lib/part_1.rb
@@ -1,5 +1,6 @@
 require "byebug"
 
+# 1
 def select_even_nums(arr)
     # even = arr.select do |ele|
     #     ele.even?
@@ -9,8 +10,41 @@ def select_even_nums(arr)
     even
 end
 
+# 2
 def reject_puppies(arr)
     older_pups = arr.reject {|k| k["age"] <= 2}
 
     older_pups
 end
+
+# 3
+def count_positive_subarrays(array)
+    # added = array.map do |ele|
+    #     ele.sum
+    # end
+ 
+    # counted = added.count { |ele| ele >= 1}
+    # # debugger
+    # return counted
+
+    # Given Solution
+    array.count { |subarr| subarr.sum > 0 }
+end
+
+# 4
+def aba_translate(str)
+    vowels = "aeiou"
+    # arr = str.split('')
+    translated = str.each_char do |char|
+        if vowels.include?(char) 
+            str.split(char)
+        # else
+        #     char
+        end
+    end
+    
+    translated
+end
+
+
+
