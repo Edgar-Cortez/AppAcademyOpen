ba501c0a6b2a0c1acbf66385529d950bd17ffa6c
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-02 09:13:41 -0500

    Completed word_changer

--- 02-Software Engineering Foundations/05-BlocksAndProcs/procs_project/lib/part_2.rb
+++ 02-Software Engineering Foundations/05-BlocksAndProcs/procs_project/lib/part_2.rb
@@ -7,3 +7,13 @@ def reverser(str, &prc)
     new_str
 end
 
+# 2
+def word_changer(str, &prc)
+    words = str.split(' ')
+    
+    new_str = words.map do |word|
+        prc.call(word)
+    end
+
+    new_str.join(' ')
+end
