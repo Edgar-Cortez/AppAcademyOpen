5419ded6b778f2b19e5f2f344701260bd81c4663
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-03 14:36:54 -0500

    Completed dog project

--- 02-Software Engineering Foundations/06-Classes/dog_project/lib/dog.rb
+++ 02-Software Engineering Foundations/06-Classes/dog_project/lib/dog.rb
@@ -1,5 +1,51 @@
 require "byebug"
 
 class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
 
+    # getters
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def bark
+        if @age > 3
+            @bark.upcase
+        else
+            @bark.downcase
+        end
+    end
+    
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(string)
+        new_food = string.capitalize
+        
+        if @favorite_foods.include?(new_food)
+            true
+        else
+            false
+        end 
+    end
+
+    # setters
+    def age=(num)
+        @age = num
+    end
 end
