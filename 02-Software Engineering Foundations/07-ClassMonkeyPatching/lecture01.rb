b62fc9f3de14a9442ec86a2de65db836a4af9ae3
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-04 15:31:03 -0500

    Completed Lecture

--- 02-Software Engineering Foundations/07-ClassMonkeyPatching/lecture01.rb
+++ 02-Software Engineering Foundations/07-ClassMonkeyPatching/lecture01.rb
@@ -1,1 +1,42 @@
 # Monkey Patching Lecture
+# - Adding additional methods to an existing class
+# Ruby Types are really classes:  Integer, String, Array, Hash, etc...
+
+
+class String
+    def upcase?
+        self.upcase == self
+    end
+end
+
+p "hello".upcase? # false
+p "HELLO".upcase? # true
+
+
+class Integer
+    def prime?
+        return false if  self < 2
+      
+        (2...self).each do |factor|
+            if self % factor == 0
+                false
+            end
+      end 
+      
+      true
+    end
+end
+
+p 7.prime? # true
+p 11.prime? # true
+p 12.prime? # false
+
+
+class Array
+    def has_zero?
+        self.include?(0)
+    end
+end
+
+p [4, 2, 3, 0, "hello"].has_zero? # true
+p [4, 2, 3, "hello"].has_zero? # false
