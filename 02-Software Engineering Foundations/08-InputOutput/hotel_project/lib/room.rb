fd1885066c4e6d7fc9c6aeff33ae7aeb55bc78c2
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-17 16:28:32 -0500

    Completed part 1

--- 02-Software Engineering Foundations/08-InputOutput/hotel_project/lib/room.rb
+++ 02-Software Engineering Foundations/08-InputOutput/hotel_project/lib/room.rb
@@ -1,3 +1,38 @@
 class Room
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length < @capacity
+      return false
+    else
+      return true
+    end
+  end
+
+  def available_space
+    return capacity - occupants.length
+  end
+
+  def add_occupant(name)
+    available = self.full?
+
+    if !available
+      @occupants << name
+      true
+    else
+      false
+    end
+  end
 
 end
