d95d558247d7891714cfe69354b730ac02b5c724
EdgarCortez1 <ea.cortezz@gmail.com>
2020-09-17 16:06:13 -0500

    Completed hangman project. Retry 1st method

--- 02-Software Engineering Foundations/08-InputOutput/hangman_project/lib/hangman.rb
+++ 02-Software Engineering Foundations/08-InputOutput/hangman_project/lib/hangman.rb
@@ -48,11 +48,86 @@ class Hangman
     matching_indices
   end
   
-
   def fill_indices(char, indices)
+    # Given Solution
     indices.each do |index|
       @guess_word[index] = char
     end
+  end
+
+  def try_guess(char)
+    # matching_idcs = get_matching_indices(char)
+    # fill_indices(char, matching_idcs)
+
+    # if already_attempted?(char)
+    #   print 'that has already beet attempted'
+    #   return false
+    # else
+    #   @attempted_chars << char
+    #   return true
+    # end
+
+    # if !@random_word.include?(char)
+    #   @remaining_incorrect_guesses -= 1
+    # end
+
+    # Given Solution
+    if self.already_attempted?(char)
+      puts 'that has already beet attempted'
+      return false
+    end
+
+    @attempted_chars << char
+
+    matches = self.get_matching_indices(char)
+    self.fill_indices(char, matches)
+
+    # if matches.empty?
+    #   @remaining_incorrect_guesses -= 1
+    # end
+
+    @remaining_incorrect_guesses -= 1 if matches.empty?
+      
+    true
+  end
+
+  def ask_user_for_guess
+    puts 'Enter a char: '
+    # char =  gets.chomp
+
+    # self.try_guess(char)
+    self.try_guess(gets.chomp)
+  end
+
+  def win?
+    if @guess_word.join('') == @secret_word
+      puts 'WIN'
+      return true
+    else
+      return false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts 'LOSE'
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    won = self.win?
+    lost = self.lose?
+
+    if won == true || lost == true
+      puts @secret_word
+      return true
+    else
+      return false
+    end
+
   end
 end
 
