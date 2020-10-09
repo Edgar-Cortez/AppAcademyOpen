24117b19f3bb763f2d27b24b3a5448f17553190b
EdgarCortez1 <ea.cortezz@gmail.com>
2020-10-09 15:28:58 -0500

    Completed part 3

--- 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/mastermind.rb
+++ 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+      @secret_code = Code.random(length)
+  end
 
+  def print_matches(input)
+    print @secret_code.num_exact_matches(input)
+    print @secret_code.num_near_matches(input)
+  end
+
+  def ask_user_for_guess
+    print "Enter a code"
+    input = gets.chomp
+
+    converted = Code.from_string(input)
+    print print_matches(input)
+
+    return converted == @secret_code
+  end
 end
