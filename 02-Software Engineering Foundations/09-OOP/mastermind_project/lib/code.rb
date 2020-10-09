811ce468bae747af22a06b674cf73d27b22e51ce
EdgarCortez1 <ea.cortezz@gmail.com>
2020-10-09 15:15:57 -0500

    Completed part 2

--- 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/code.rb
+++ 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/code.rb
@@ -47,4 +47,50 @@ class Code
   def length
     @pegs.length
   end
+
+  def num_exact_matches(guess)
+    # count = 0
+    # default_pegs = POSSIBLE_PEGS.keys
+    # guessed = Code.new(guess)
+    
+    # guess.each do |peg|
+    #   if guess[peg] == @pegs[peg]
+    #     count += 1
+    #   end
+    # end
+
+    # count
+
+    # Given Solution
+    count = 0
+
+    (0...guess.length).each do |peg|
+      count += 1 if guess[peg] == @pegs[peg]
+    end
+
+    count
+  end
+
+  def num_near_matches(guess)
+    # count = 0
+
+    # @pegs.any? { |peg| count+= 1 if guess.include?(peg) }
+    
+    # count
+
+    # Given Solution
+    count = 0
+
+    (0...guess.length).each do |peg|
+      if guess[peg] != self[peg] && self.pegs.include?(guess[peg])
+        count += 1
+      end
+    end
+
+    count
+  end
+
+  def ==(input)
+    input == self.pegs
+  end
 end
