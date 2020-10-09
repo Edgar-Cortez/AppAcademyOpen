0c7a60a672fcf55d54a8517817f8d93b9c84b2fe
EdgarCortez1 <ea.cortezz@gmail.com>
2020-10-09 15:39:13 -0500

    Added given solutions

--- 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/mastermind.rb
+++ 02-Software Engineering Foundations/09-OOP/mastermind_project/lib/mastermind.rb
@@ -6,17 +6,27 @@ class Mastermind
   end
 
   def print_matches(input)
-    print @secret_code.num_exact_matches(input)
-    print @secret_code.num_near_matches(input)
+    # print @secret_code.num_exact_matches(input)
+    # print @secret_code.num_near_matches(input)
+
+    # Given Solution
+    puts "exact matches: #{@secret_code.num_exact_matches(input)}"
+    puts "near matches: #{@secret_code.num_near_matches(input)}"
   end
 
   def ask_user_for_guess
-    print "Enter a code"
-    input = gets.chomp
+    # print "Enter a code: "
+    # input = gets.chomp
 
-    converted = Code.from_string(input)
-    print print_matches(input)
+    # converted = Code.from_string(input)
+    # print print_matches(input)
 
-    return converted == @secret_code
+    # return converted == @secret_code
+
+    # Given Solution
+    print "Enter a code: "
+    guess = Code.from_string(gets.chomp)
+    self.print_matches(guess)
+    @secret_code == guess
   end
 end
