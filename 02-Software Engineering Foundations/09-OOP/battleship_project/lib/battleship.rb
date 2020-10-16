0c0354e956ce25f385ebf2f834c6ae62c507faaa
EdgarCortez1 <ea.cortezz@gmail.com>
2020-10-16 17:13:55 -0500

    Completed part 3

--- 02-Software Engineering Foundations/09-OOP/battleship_project/lib/battleship.rb
+++ 02-Software Engineering Foundations/09-OOP/battleship_project/lib/battleship.rb
@@ -2,5 +2,60 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size * 0.50
+  end
 
+  def board
+    @board
+  end
+
+  def player
+    @player
+  end
+
+  def start_game
+    @board.place_random_ships
+    print @board.num_ships
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      print "you lose"
+      return true
+    else
+      return false      
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      print "you win"
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    if self.lose? == true || self.win? == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def turn    
+    if @board.attack(@player.get_move) == true
+      @remaining_misses
+    else
+      @remaining_misses -= 1
+    end
+
+    @board.print
+    print "Remaining misses: #{@remaining_misses}"
+  end
 end
--- 02-Software Engineering Foundations/09-OOP/battleship_project/lib/player.rb
+++ 02-Software Engineering Foundations/09-OOP/battleship_project/lib/player.rb
@@ -1,3 +1,17 @@
 class Player
+  def get_move
+    # print "enter a position with coordinates separated with a space like `4 7`"
+    # position = gets.chomp.split(" ")
 
+    # new_coord = position.map do |ele|
+    #   ele.to_i
+    # end
+    
+    # new_coord
+
+    # Given Solution
+    print "enter a position with coordinates separated with a space like `4 7`"
+    gets.chomp.split(" ").map(&:to_i)
+    
+  end
 end
