3a1f9126dfec396064d7812b388c411cb1ff96a4
EdgarCortez1 <ea.cortezz@gmail.com>
2020-10-16 16:27:42 -0500

    Completed part 2

--- 02-Software Engineering Foundations/09-OOP/battleship_project/lib/board.rb
+++ 02-Software Engineering Foundations/09-OOP/battleship_project/lib/board.rb
@@ -1,6 +1,12 @@
 class Board
   attr_reader (:size)
 
+  def self.print_grid(grid)
+    grid.each do |ele|
+      puts ele.join(" ")        
+    end
+  end
+
   def initialize(n)
     @grid = Array.new(n) { Array.new(n, :N)}
     @size = n * n
@@ -29,5 +35,51 @@ class Board
 
     # Given Solution
     @grid.flatten.count { |ele| ele == :S }
+  end
+
+  def attack(position)    
+    if self[position] == :S
+      self[position] = :H
+      p "You sunk my battleship!"
+      return true
+    else
+      self[position] = :X
+      return false
+    end
+  end
+
+  def place_random_ships
+  # Given Solution 
+    total_ships = @size * 0.25    # getting 25% of the total ships on the grid
+    
+    while self.num_ships < total_ships    
+      rand_row = rand(0...@grid.length)   
+      rand_col = rand(0...@grid.length)
+      pos = [rand_row, rand_col]
+      self[pos] = :S
+    end
+  end
+
+  def hidden_ships_grid
+    # I was close. Didn't know I had to use map on each loop. Also, to return :N instead of set ele = :N
+    @grid.map do |sub|
+      sub.map do |ele|
+        if ele == :S
+          :N
+        else
+          ele
+        end
+      end
+    end
+  end
+
+  def cheat
+    # Was using self instead of Board. Remember to try exactly what it wants me to call first!
+    Board.print_grid(@grid)
+  end
+
+  def print
+    # Was using self instead of Board. Remember to try exactly what it wants me to call first!
+    Board.print_grid(self.hidden_ships_grid)
   end
 end
