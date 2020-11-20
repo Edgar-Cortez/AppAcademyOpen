a0c5d8bbbe40db60bd8438a32786a1800ed48e8a
EdgarCortez1 <ea.cortezz@gmail.com>
2020-11-20 11:34:38 -0600

    redo ex05

--- 02-Software Engineering Foundations/10-FoundationsRecap/recap_exercise_1/lib/problems.rb
+++ 02-Software Engineering Foundations/10-FoundationsRecap/recap_exercise_1/lib/problems.rb
@@ -82,7 +82,21 @@ class String
   #
   # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
   # "cats".substrings(2)  # => ["ca", "at", "ts"]
-  def substrings(length = nil); end
+  def substrings(length = nil)
+    subs = []
+    (0...self.length).each do |start_idx|
+      (start_idx..self.length).each do |end_idx|
+        sub =self[start_idx..end_idx]
+        subs << sub
+      end
+    end
+
+    if length.nil?
+      subs
+    else
+      subs.select{ |str| str.length == length }
+    end
+  end
 
 
   # Write a method, String#caesar_cipher, that takes in an a number.
@@ -94,5 +108,7 @@ class String
   # "apple".caesar_cipher(1)    #=> "bqqmf"
   # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
   # "zebra".caesar_cipher(4)    #=> "difve"
-  def caesar_cipher(num); end
+  def caesar_cipher(num)
+  
+  end
 end
