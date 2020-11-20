207d6ef38ed79245bab21fec8bb4d07bcbd0f76a
EdgarCortez1 <ea.cortezz@gmail.com>
2020-11-20 16:35:23 -0600

    Completed ex01

--- 02-Software Engineering Foundations/10-FoundationsRecap/recap_exercise_3/problems-general.rb
+++ 02-Software Engineering Foundations/10-FoundationsRecap/recap_exercise_3/problems-general.rb
@@ -1,6 +1,19 @@
+# General Problems
+
+# Write a method no_dupes?(arr) that accepts an array as an arg and returns an new array containing the elements that were not repeated in the array.
 def no_dupes?(arr)
-  
+  ele_count = arr.each_with_object(Hash.new(0)) do |ele, new_hash|
+    new_hash[ele] += 1
+  end
+
+  ele_count.select{ |k, v| v == 1}.keys
 end
+
+# Examples
+p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+p no_dupes?([true, true, true])            # => []
+
 
 def no_consecutive_repeats?(arr)
   
